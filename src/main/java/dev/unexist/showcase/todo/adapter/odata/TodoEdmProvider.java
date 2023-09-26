71a01a09a6c96f5e1e366b60d218b7de3b30a956
Christoph Kappel <christoph@unexist.dev>
2023-09-26 14:01:27 +0200

    Renamed files to match scheme

--- src/main/java/dev/unexist/showcase/todo/adapter/odata/TodoEdmProvider.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/odata/TodoEdmProvider.java
@@ -9,7 +9,7 @@
  * See the file LICENSE for details.
  **/
 
-package dev.unexist.showcase.todo.adapter;
+package dev.unexist.showcase.todo.adapter.odata;
 
 import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind;
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
@@ -22,21 +22,19 @@ import org.apache.olingo.commons.api.edm.provider.CsdlProperty;
 import org.apache.olingo.commons.api.edm.provider.CsdlPropertyRef;
 import org.apache.olingo.commons.api.edm.provider.CsdlSchema;
 
-import javax.enterprise.context.ApplicationScoped;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@ApplicationScoped
-public class EdmProvider extends CsdlAbstractEdmProvider {
+public class TodoEdmProvider extends CsdlAbstractEdmProvider {
     public static final String NAMESPACE = "OData.Demo";
     public static final String CONTAINER_NAME = "Container";
-    public static final String ET_PRODUCT_NAME = "Todo";
-    public static final String ES_PRODUCTS_NAME = "Todos";
+    public static final String ET_TODO_NAME = "Todo";
+    public static final String ES_TODOS_NAME = "Todos";
 
     public static final FullQualifiedName CONTAINER = new FullQualifiedName(NAMESPACE, CONTAINER_NAME);
-    public static final FullQualifiedName ET_PRODUCT_FQN = new FullQualifiedName(NAMESPACE, ET_PRODUCT_NAME);
+    public static final FullQualifiedName ET_TODO_FQN = new FullQualifiedName(NAMESPACE, ET_TODO_NAME);
 
     @Override
     public List<CsdlSchema> getSchemas() {
@@ -44,7 +42,7 @@ public class EdmProvider extends CsdlAbstractEdmProvider {
         schema.setNamespace(NAMESPACE);
 
         List<CsdlEntityType> entityTypes = new ArrayList<>();
-        entityTypes.add(getEntityType(ET_PRODUCT_FQN));
+        entityTypes.add(getEntityType(ET_TODO_FQN));
 
         schema.setEntityTypes(entityTypes);
         schema.setEntityContainer(getEntityContainer());
@@ -60,7 +58,7 @@ public class EdmProvider extends CsdlAbstractEdmProvider {
         CsdlEntityType retVal = null;
 
         // this method is called for one of the EntityTypes that are configured in the Schema
-        if (entityTypeName.equals(ET_PRODUCT_FQN)) {
+        if (ET_TODO_FQN.equals(entityTypeName)) {
             CsdlProperty id = new CsdlProperty()
                     .setName("ID")
                     .setType(EdmPrimitiveTypeKind.Int32.getFullQualifiedName());
@@ -78,7 +76,7 @@ public class EdmProvider extends CsdlAbstractEdmProvider {
             // configure EntityType
             CsdlEntityType entityType = new CsdlEntityType();
 
-            entityType.setName(ET_PRODUCT_NAME);
+            entityType.setName(ET_TODO_NAME);
             entityType.setProperties(Arrays.asList(id, name, description));
             entityType.setKey(Collections.singletonList(propertyRef));
 
@@ -92,11 +90,12 @@ public class EdmProvider extends CsdlAbstractEdmProvider {
     public CsdlEntitySet getEntitySet(FullQualifiedName entityContainer, String entitySetName) {
         CsdlEntitySet retVal = null;
 
-        if (entityContainer.equals(CONTAINER)) {
-            if (entitySetName.equals(ES_PRODUCTS_NAME)) {
+        if (CONTAINER.equals(entityContainer)) {
+            if (ES_TODOS_NAME.equals(entitySetName)) {
                 CsdlEntitySet entitySet = new CsdlEntitySet();
-                entitySet.setName(ES_PRODUCTS_NAME);
-                entitySet.setType(ET_PRODUCT_FQN);
+
+                entitySet.setName(ES_TODOS_NAME);
+                entitySet.setType(ET_TODO_FQN);
 
                 retVal = entitySet;
             }
@@ -108,7 +107,7 @@ public class EdmProvider extends CsdlAbstractEdmProvider {
     @Override
     public CsdlEntityContainer getEntityContainer() {
         List<CsdlEntitySet> entitySets = new ArrayList<>();
-        entitySets.add(getEntitySet(CONTAINER, ES_PRODUCTS_NAME));
+        entitySets.add(getEntitySet(CONTAINER, ES_TODOS_NAME));
 
         CsdlEntityContainer entityContainer = new CsdlEntityContainer();
 
@@ -123,7 +122,7 @@ public class EdmProvider extends CsdlAbstractEdmProvider {
         CsdlEntityContainerInfo retVal = null;
 
         // This method is invoked when displaying the service document at e.g. http://localhost:8080/DemoService/DemoService.svc
-        if (null == entityContainerName || entityContainerName.equals(CONTAINER)) {
+        if (null == entityContainerName || CONTAINER.equals(entityContainerName)) {
             CsdlEntityContainerInfo entityContainerInfo = new CsdlEntityContainerInfo();
             entityContainerInfo.setContainerName(CONTAINER);
 
