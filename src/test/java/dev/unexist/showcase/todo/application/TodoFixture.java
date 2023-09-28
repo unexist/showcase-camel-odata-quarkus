f583b11b5f62bd84a1ad592f2288f5ac16dfa98f
Christoph Kappel <christoph@unexist.dev>
2023-09-28 16:38:02 +0200

    Added helper #createEntityJSON

--- src/test/java/dev/unexist/showcase/todo/application/TodoFixture.java
+++ src/test/java/dev/unexist/showcase/todo/application/TodoFixture.java
@@ -11,6 +11,9 @@
 
 package dev.unexist.showcase.todo.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.unexist.showcase.todo.domain.todo.Todo;
 import dev.unexist.showcase.todo.domain.todo.TodoFactory;
 
@@ -21,5 +24,16 @@ public class TodoFixture {
         return TodoFactory.createTodoFromData(0,
                 "string", "string", false,
                 DATE, DATE);
+    }
+
+    public static String createEntityJSON() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        ObjectNode root = mapper.createObjectNode();
+
+        root.put("Title", "string");
+        root.put("Description", "string");
+
+        return mapper.writeValueAsString(root);
     }
 }
