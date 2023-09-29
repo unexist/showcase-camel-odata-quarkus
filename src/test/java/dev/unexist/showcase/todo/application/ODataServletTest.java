2ec27fa909bc5be73e12eea49d023d946a75f31f
Christoph Kappel <christoph@unexist.dev>
2023-09-29 11:01:35 +0200

    Removed trailing spaces

--- src/test/java/dev/unexist/showcase/todo/application/ODataServletTest.java
+++ src/test/java/dev/unexist/showcase/todo/application/ODataServletTest.java
@@ -4,8 +4,8 @@
  * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
- *         This program can be distributed under the terms of the Apache License v2.0.
- *         See the file LICENSE for details.
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
  **/
 
 package dev.unexist.showcase.todo.application;
