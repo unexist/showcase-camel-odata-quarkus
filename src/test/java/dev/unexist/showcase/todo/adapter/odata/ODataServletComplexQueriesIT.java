8e31ad4450c6461817c6d975c3ce15e7b5833dfe
Christoph Kappel <christoph@unexist.dev>
2024-02-15 15:29:44 +0100

    Updated test data

--- src/test/java/dev/unexist/showcase/todo/adapter/odata/ODataServletComplexQueriesIT.java
+++ src/test/java/dev/unexist/showcase/todo/adapter/odata/ODataServletComplexQueriesIT.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 
 @QuarkusTest
 public class ODataServletComplexQueriesIT extends ODataServletBaseIT {
@@ -28,16 +29,28 @@ public class ODataServletComplexQueriesIT extends ODataServletBaseIT {
         String jsonOut = given()
                 .when()
                     .accept(ContentType.JSON)
-                    .get("/odata/Todos(1)?$select=Title&$expand=Task($select=Title)")
+                    .get("/odata/Todos(1)?$select=Title&$expand=Tasks($select=Title)")
                 .then()
                     .statusCode(200)
                 .and()
                     .extract()
                     .asString();
 
+        final Object expectedObject = json(String.join(System.lineSeparator(),
+                    "{",
+                    "\"@odata.id\": \"${json-unit.any-string}\",",
+                    "\"ID\": \"${json-unit.any-number}\",",
+                    "\"Title\": \"${json-unit.any-string}\"",
+                    "}"));
+
         assertThatJson(jsonOut)
-                .inPath("$.Task")
-                    .isObject()
-                    .isNotEmpty();
+                .isObject()
+                    .node("Title").isString();
+
+        assertThatJson(jsonOut)
+                .inPath("$.Tasks")
+                    .isArray()
+                    .isNotEmpty()
+                    .allSatisfy(elem -> assertThatJson(elem).isEqualTo(expectedObject));
     }
 }
