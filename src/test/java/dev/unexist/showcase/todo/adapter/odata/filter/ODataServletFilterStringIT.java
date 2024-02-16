ecde45dbf6cb4f3b68622b7e5e6d9f0d3d380e63
Christoph Kappel <christoph@unexist.dev>
2024-02-16 11:54:15 +0100

    Added tests for filter by string

---
+++ src/test/java/dev/unexist/showcase/todo/adapter/odata/filter/ODataServletFilterStringIT.java
@@ -0,0 +1,45 @@
+/**
+ * @package Showcase-OData-Quarkus
+ *
+ * @file
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.adapter.odata.filter;
+
+import dev.unexist.showcase.todo.adapter.odata.ODataServletBaseIT;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+
+@QuarkusTest
+public class ODataServletFilterStringIT extends ODataServletBaseIT {
+
+    @Test
+    public void shouldFilterString() {
+        String jsonOut = given()
+                .when()
+                    .accept(ContentType.JSON)
+                    .get("/odata/Todos?$filter=Title eq 'Todo string'")
+                .then()
+                    .statusCode(200)
+                .and()
+                    .extract()
+                    .asString();
+
+        System.out.println(jsonOut);
+
+        assertThatJson(jsonOut)
+                .inPath("$.value..[\"ID\"]")
+                    .isArray()
+                    .isEqualTo(json("[3,2,1]"));
+    }
+}
