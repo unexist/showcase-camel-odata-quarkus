a53d90b7af4497838fa9b281c5f8b47b54b9dd72
Christoph Kappel <christoph@unexist.dev>
2024-02-16 11:54:25 +0100

    Added tests for filter boolean operators

---
+++ src/test/java/dev/unexist/showcase/todo/adapter/odata/filter/ODataServletFilterBooleanIT.java
@@ -0,0 +1,66 @@
+/**
+ * @package Showcase-OData-Quarkus
+ *
+ * @file
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.adapter.odata.filter;
+
+import dev.unexist.showcase.todo.adapter.odata.ODataServletBaseIT;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+
+@QuarkusTest
+public class ODataServletFilterBooleanIT extends ODataServletBaseIT {
+
+    @Test
+    public void shouldFilterBoolAnd() {
+        String jsonOut = given()
+                .when()
+                    .accept(ContentType.JSON)
+                    .get("/odata/Todos?$filter=contains(Title, 'Todo') and ID eq 1")
+                .then()
+                    .statusCode(200)
+                .and()
+                    .extract()
+                    .asString();
+
+        System.out.println(jsonOut);
+
+        assertThatJson(jsonOut)
+                .inPath("$.value..[\"ID\"]")
+                    .isArray()
+                    .isEqualTo(json("[3,2,1]"));
+    }
+
+
+    @Test
+    public void shouldFilterBoolOr() {
+        String jsonOut = given()
+                .when()
+                    .accept(ContentType.JSON)
+                    .get("/odata/Todos?$filter=contains(Title, 'Todo') or ID eq 1")
+                .then()
+                    .statusCode(200)
+                .and()
+                    .extract()
+                    .asString();
+
+        System.out.println(jsonOut);
+
+        assertThatJson(jsonOut)
+                .inPath("$.value..[\"ID\"]")
+                    .isArray()
+                    .isEqualTo(json("[3,2,1]"));
+    }
+}
