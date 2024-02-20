840a01a750128df46fdc54a4bd3385222cf61f5a
Christoph Kappel <christoph@unexist.dev>
2024-02-20 17:45:57 +0100

    Extracted #expandEntities to EntityProcessorBase

--- src/main/java/dev/unexist/showcase/todo/adapter/odata/processor/EntityProcessor.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/odata/processor/EntityProcessor.java
@@ -11,18 +11,12 @@
 
 package dev.unexist.showcase.todo.adapter.odata.processor;
 
-import dev.unexist.showcase.todo.adapter.odata.entity.TaskEntityService;
 import dev.unexist.showcase.todo.adapter.odata.storage.EntityStorage;
-import org.apache.olingo.commons.api.Constants;
 import org.apache.olingo.commons.api.data.ContextURL;
 import org.apache.olingo.commons.api.data.Entity;
-import org.apache.olingo.commons.api.data.EntityCollection;
-import org.apache.olingo.commons.api.data.Link;
-import org.apache.olingo.commons.api.edm.EdmElement;
 import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
 import org.apache.olingo.commons.api.edm.EdmNavigationProperty;
-import org.apache.olingo.commons.api.edm.EdmNavigationPropertyBinding;
 import org.apache.olingo.commons.api.format.ContentType;
 import org.apache.olingo.commons.api.http.HttpHeader;
 import org.apache.olingo.commons.api.http.HttpMethod;
@@ -42,7 +36,6 @@ import org.apache.olingo.server.api.uri.UriParameter;
 import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceEntitySet;
 import org.apache.olingo.server.api.uri.UriResourceNavigation;
-import org.apache.olingo.server.api.uri.queryoption.ExpandItem;
 import org.apache.olingo.server.api.uri.queryoption.ExpandOption;
 import org.apache.olingo.server.api.uri.queryoption.SelectOption;
 
@@ -102,65 +95,7 @@ public class EntityProcessor extends EntityProcessorBase
             expandOption = uriInfo.getExpandOption();
 
             if (null != expandOption) {
-                EdmNavigationProperty edmNavigationProperty = null;
-                ExpandItem expandItem = expandOption.getExpandItems().get(0);
-
-                if (expandItem.isStar()) {
-                    List<EdmNavigationPropertyBinding> bindings =
-                            startEdmEntitySet.getNavigationPropertyBindings();
-
-                    if (!bindings.isEmpty()) {
-                        /* Lookup bindings */
-                        for (EdmNavigationPropertyBinding binding : bindings) {
-                            EdmElement property = startEdmEntitySet.getEntityType()
-                                    .getProperty(binding.getPath());
-
-                            EdmElement property2 = startEdmEntitySet.getEntityType()
-                                    .getProperty(TaskEntityService.ES_NAME);
-
-                            if(property instanceof EdmNavigationProperty) {
-                                edmNavigationProperty = (EdmNavigationProperty) property;
-
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    UriResource expandUriResource = expandItem.getResourcePath()
-                            .getUriResourceParts().get(0);
-
-                    if(expandUriResource instanceof UriResourceNavigation) {
-                        edmNavigationProperty = ((UriResourceNavigation) expandUriResource).getProperty();
-                    }
-                }
-
-                if (null != edmNavigationProperty) {
-                    String navPropName = edmNavigationProperty.getName();
-                    EdmEntityType expandEdmEntityType = edmNavigationProperty.getType();
-
-                    /* Build the inline data */
-                    Link link = new Link();
-
-                    link.setTitle(navPropName);
-                    link.setType(Constants.ENTITY_NAVIGATION_LINK_TYPE);
-                    link.setRel(Constants.NS_ASSOCIATION_LINK_REL + navPropName);
-
-                    if (edmNavigationProperty.isCollection()) {
-                        EntityCollection expandEntityCollection = this.storage.getRelatedEntityCollection(
-                                responseEntity, expandEdmEntityType);
-
-                        link.setInlineEntitySet(expandEntityCollection);
-                        link.setHref(expandEntityCollection.getId().toASCIIString());
-                    } else {
-                        Entity expandEntity = this.storage.getRelatedEntity(
-                                responseEntity, expandEdmEntityType);
-
-                        link.setInlineEntity(expandEntity);
-                        link.setHref(expandEntity.getId().toASCIIString());
-                    }
-
-                    responseEntity.getNavigationLinks().add(link);
-                }
+                expandEntities(expandOption, startEdmEntitySet, responseEntity);
             }
         } else if (segmentCount == 2) {
             UriResource navSegment = resourceParts.get(1);
--- src/main/java/dev/unexist/showcase/todo/adapter/odata/processor/EntityProcessorBase.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/odata/processor/EntityProcessorBase.java
@@ -11,10 +11,18 @@
 
 package dev.unexist.showcase.todo.adapter.odata.processor;
 
+import dev.unexist.showcase.todo.adapter.odata.entity.TaskEntityService;
 import dev.unexist.showcase.todo.adapter.odata.storage.EntityStorage;
+import org.apache.olingo.commons.api.Constants;
+import org.apache.olingo.commons.api.data.Entity;
+import org.apache.olingo.commons.api.data.EntityCollection;
+import org.apache.olingo.commons.api.data.Link;
 import org.apache.olingo.commons.api.edm.EdmBindingTarget;
+import org.apache.olingo.commons.api.edm.EdmElement;
 import org.apache.olingo.commons.api.edm.EdmEntitySet;
+import org.apache.olingo.commons.api.edm.EdmEntityType;
 import org.apache.olingo.commons.api.edm.EdmNavigationProperty;
+import org.apache.olingo.commons.api.edm.EdmNavigationPropertyBinding;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.OData;
 import org.apache.olingo.server.api.ODataApplicationException;
@@ -22,6 +30,8 @@ import org.apache.olingo.server.api.ServiceMetadata;
 import org.apache.olingo.server.api.uri.UriInfo;
 import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceNavigation;
+import org.apache.olingo.server.api.uri.queryoption.ExpandItem;
+import org.apache.olingo.server.api.uri.queryoption.ExpandOption;
 
 import java.util.List;
 import java.util.Locale;
@@ -81,5 +91,69 @@ abstract public class EntityProcessorBase {
         }
 
         return navigationTargetEntitySet;
+    }
+
+    protected void expandEntities(ExpandOption expandOption, EdmEntitySet startEdmEntitySet, Entity responseEntity) {
+        if (null != expandOption) {
+            EdmNavigationProperty edmNavigationProperty = null;
+            ExpandItem expandItem = expandOption.getExpandItems().get(0);
+
+            if (expandItem.isStar()) {
+                List<EdmNavigationPropertyBinding> bindings =
+                        startEdmEntitySet.getNavigationPropertyBindings();
+
+                if (!bindings.isEmpty()) {
+                    /* Lookup bindings */
+                    for (EdmNavigationPropertyBinding binding : bindings) {
+                        EdmElement property = startEdmEntitySet.getEntityType()
+                                .getProperty(binding.getPath());
+
+                        EdmElement property2 = startEdmEntitySet.getEntityType()
+                                .getProperty(TaskEntityService.ES_NAME);
+
+                        if(property instanceof EdmNavigationProperty) {
+                            edmNavigationProperty = (EdmNavigationProperty) property;
+
+                            break;
+                        }
+                    }
+                }
+            } else {
+                UriResource expandUriResource = expandItem.getResourcePath()
+                        .getUriResourceParts().get(0);
+
+                if(expandUriResource instanceof UriResourceNavigation) {
+                    edmNavigationProperty = ((UriResourceNavigation) expandUriResource).getProperty();
+                }
+            }
+
+            if (null != edmNavigationProperty) {
+                String navPropName = edmNavigationProperty.getName();
+                EdmEntityType expandEdmEntityType = edmNavigationProperty.getType();
+
+                /* Build the inline data */
+                Link link = new Link();
+
+                link.setTitle(navPropName);
+                link.setType(Constants.ENTITY_NAVIGATION_LINK_TYPE);
+                link.setRel(Constants.NS_ASSOCIATION_LINK_REL + navPropName);
+
+                if (edmNavigationProperty.isCollection()) {
+                    EntityCollection expandEntityCollection = this.storage.getRelatedEntityCollection(
+                            responseEntity, expandEdmEntityType);
+
+                    link.setInlineEntitySet(expandEntityCollection);
+                    link.setHref(expandEntityCollection.getId().toASCIIString());
+                } else {
+                    Entity expandEntity = this.storage.getRelatedEntity(
+                            responseEntity, expandEdmEntityType);
+
+                    link.setInlineEntity(expandEntity);
+                    link.setHref(expandEntity.getId().toASCIIString());
+                }
+
+                responseEntity.getNavigationLinks().add(link);
+            }
+        }
     }
 }
