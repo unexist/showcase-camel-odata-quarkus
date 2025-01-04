f97bfd5aa3cc34665e73538949e606ebd81aa4be
Christoph Kappel <christoph@unexist.dev>
2025-01-04 20:41:50 +0100

    Fixed doc header

--- src/main/java/dev/unexist/showcase/todo/adapter/odata/entity/TodoEntityService.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/odata/entity/TodoEntityService.java
@@ -1,11 +1,12 @@
 /**
- * @package Showcase-showcase-odata-quarkus
- * @file
+ * @package Showcase-OData-Quarkus
+ *
+ * @file OData todo entity service
  * @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
- *         This program can be distributed under the terms of the Apache License v2.0.
- *         See the file LICENSE for details.
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
  **/
 
 package dev.unexist.showcase.todo.adapter.odata.entity;
